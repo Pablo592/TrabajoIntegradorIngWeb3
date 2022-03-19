482282a124071d2a08abc1e0f858a95e60f0cc53
Joelspitale <vspitale107@alumnos.iua.edu.ar>
2022-03-19 18:21:13 -0300

    perfil activo listo

---
+++ src/main/java/ar/edu/iua/iw3/web/WebDefaultController.java
@@ -0,0 +1,13 @@
+package ar.edu.iua.iw3.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebDefaultController { //lo utilizo para hacer mapeo a un recurrso estatico
+
+    @GetMapping(value = "/")
+    public String defaultPage(){
+        return "redirect:/index.html";  //devuelvo una directiva http, que es un status code = 302 + una cabecera location de http
+    }
+}
