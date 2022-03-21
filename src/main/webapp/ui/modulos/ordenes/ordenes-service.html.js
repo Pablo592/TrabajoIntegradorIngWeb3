7d6276a07788b650b5c3a0d60823323b9ae8b555
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-03-20 23:36:18 -0300

    Anda el actualizar orden
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- src/main/webapp/ui/modulos/ordenes/ordenes-service.html.js
+++ src/main/webapp/ui/modulos/ordenes/ordenes-service.html.js
@@ -9,8 +9,24 @@ angular.module('ordenes').factory('OrdenesService',function($http, URL_BASE){
         edit:function(p) {
             return $http.put(URL_BASE+'/ordenes',p);
         },
+
+        establecerTara:function(p) {
+            return $http.put(URL_BASE+'/ordenes/tara',p);  //Segundo envio
+        },
+
+        frenarCarga:function(p) {
+            return $http.put(URL_BASE+'/ordenes/frenar-carga',p);  //Tercer envio
+        },
+
+        pesoFinal:function(p) {
+            return $http.put(URL_BASE+'/ordenes/peso-final',p);  //Cuarto envio
+        },
+
         remove:function(p) {
             return $http.delete(URL_BASE+'/ordenes/'+p.id);
         }
     };
 });
+
+
+
