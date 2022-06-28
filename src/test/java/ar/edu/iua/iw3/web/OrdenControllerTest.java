41b1e4a785be1118659fa21515da8f66ae9083e9
joelSpitale <vspitale107@alumnos.iua.edu.ar>
2022-06-28 08:32:14 -0300

    obtener conciliacion test controller

--- src/test/java/ar/edu/iua/iw3/web/OrdenControllerTest.java
+++ src/test/java/ar/edu/iua/iw3/web/OrdenControllerTest.java
@@ -10,6 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import ar.edu.iua.iw3.modelo.Cuentas.Rol;
 import ar.edu.iua.iw3.modelo.Cuentas.Usuario;
 import ar.edu.iua.iw3.modelo.Orden;
+import ar.edu.iua.iw3.modelo.dto.ConciliacionDTO;
 import ar.edu.iua.iw3.negocio.OrdenNegocio;
 import ar.edu.iua.iw3.security.authtoken.AuthToken;
 import org.junit.Before;
@@ -23,11 +24,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -71,11 +74,42 @@ public class OrdenControllerTest {
 
         //then
         mvc.perform(get("/test/api/v1/ordenes")
+                        .param("xauthtoken", token)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$",hasSize(1)))
-                .andExpect(jsonPath("$[0].id",is(orden.getId()),Long.class));
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$",hasSize(1)))
+                        .andExpect(jsonPath("$[0].id",is(orden.getId()),Long.class));
+    }
+
+
+    @Test
+    public void listOrdenById_Success() throws Exception {
+        String token = getToken();
+        float pesajeInicial = 1000;
+        float pesajeFinal = 2000;
+        float masaAcumuladaKg =1000;
+        float netoPorBalanza = 0;
+        float diferenciaNetoPorBalanza_masaAcumuludada = 1000;
+        float promedDensidadProductoKilogramoMetroCub = (float) 0.874;
+        float promedioTemperaturaProductoCelcius = 20;
+        float promedioCaudalLitroSegundo = (float) 0.16;
+
+
+
+        //given
+        ConciliacionDTO conciliacionDTO = new ConciliacionDTO(pesajeInicial,pesajeFinal,masaAcumuladaKg,netoPorBalanza,diferenciaNetoPorBalanza_masaAcumuludada,promedDensidadProductoKilogramoMetroCub,promedioTemperaturaProductoCelcius,promedioCaudalLitroSegundo);
+
+        //when
+        when(ordenNegocio.obtenerConciliacion(orden.getCodigoExterno())).thenReturn(conciliacionDTO);
+
+        //then
+        mvc.perform(get("/test/api/v1/ordenes/conciliacion/" + orden.getCodigoExterno())
+                        .param("xauthtoken", token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath(("$.pesajeInicial"),is(conciliacionDTO.getPesajeInicial()),Float.class));
     }
 
     //buscar una orden por id
