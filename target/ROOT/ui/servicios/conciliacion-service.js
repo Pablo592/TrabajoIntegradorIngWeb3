a71ff5795497423fa91e149212b24d5ecfe04bb6
joelSpitale <vspitale107@alumnos.iua.edu.ar>
2022-08-02 17:36:00 -0300

    version final

--- target/ROOT/ui/servicios/conciliacion-service.js
+++
@@ -1,7 +0,0 @@
-angular.module('conciliacion').factory('ConciliacionService', function ($http, URL_BASE) {
-    return {
-        pedirConciliacion: function (id) {
-            return $http.get(URL_BASE + '/ordenes/conciliacion/' + id);
-        }
-    };
-});
--- target/ROOT/ui/servicios/crearUsuario-service.js
+++
@@ -1,7 +0,0 @@
-angular.module('crearUsuario').factory('CrearUsuarioService', function ($http, URL_BASE) {
-    return {
-        crearUsuario: function (p) {
-            return $http.post(URL_BASE + '/usuarios',p);
-        }
-    };
-});
